27be7b4088a8644297c0acd2c0e3e7311a8eb5ad
abusedmedia <hello@fabiofranchino.com>
2021-03-01 14:54:26 +0100

    module refactor

--- src/index.js
+++ src/index.js
@@ -1,54 +1,82 @@
 import './global.css'
 import css from './style.css'
 
-const parser = new DOMParser()
+const parser = new window.DOMParser()
 
-const module = function (sceneElement, modConfig, sceneConfig, projectConfig) {
+const module = function (sceneElement, modConfig, sceneConfig) {
   const ob = sceneConfig.toast
   if (!ob) return
 
-  const delay = ob.delay || 1
+  const delay = parseInt(ob.enter) > 0 ? parseInt(ob.enter) : 0
+  const duration = parseInt(ob.exit) > 0 ? parseInt(ob.exit) : 0
+
+  let enter = 'auto'
+  if (ob.enter) enter = ob.enter
+  if (delay > 0) enter = 'auto'
+
+  let exit = 'auto'
+  if (ob.exit === 'click') exit = 'click'
+  if (duration > 0) exit = 'auto'
+
+  let dom = null
 
   const enterEffect = 'bounceIn'
-  const exitEffect = 'fadeOut'
+  const exitEffect = 'bounceOut'
 
   const show = () => {
+    sceneElement.removeEventListener('click', show)
+
     const child = `<div class="${css.toast}">
       <div class="${css.strip} animate__animated animate__${enterEffect} animate__delay-${delay}s">${ob.text}</div>
     </div>`
 
-    const dom = parser.parseFromString(child, 'text/html').body.childNodes[0]
+    dom = parser.parseFromString(child, 'text/html').body.childNodes[0]
     sceneElement.appendChild(dom)
 
     const strip = dom.querySelector('.' + css.strip)
     strip.addEventListener('click', (e) => {
-      strip.classList.remove(`animate__${enterEffect}`, `animate__delay-${delay}s`)
-      strip.classList.add(`animate__${exitEffect}`)
+      hide()
       e.stopPropagation()
       e.preventDefault()
     })
+
+    if ((exit === 'auto') && duration > 0) {
+      setTimeout(hide, (duration + delay) * 1000)
+    }
+    if (exit === 'click') {
+      sceneElement.addEventListener('click', hide)
+    }
+  }
+
+  const hide = () => {
+    if (dom) {
+      const strip = dom.querySelector('.' + css.strip)
+      strip.classList.remove(`animate__${enterEffect}`, `animate__delay-${delay}s`)
+      strip.classList.add(`animate__${exitEffect}`)
+    }
   }
 
   const showByKey = e => {
     if (e.key === ob.enter) show()
   }
 
-  if (ob.enter === 'click') {
-    sceneElement.addEventListener('click', show)
-  }
-  if (ob.enter === 'step') {
-    sceneElement.addEventListener('click', show)
-  }
-  if (typeof ob.enter === 'string') {
-    document.addEventListener('keyup', showByKey)
-  }
+  switch (enter) {
+    case 'click':
+      sceneElement.addEventListener('click', show)
+      break
 
-  if (!ob.enter || ob.enter === 'auto') {
-    show()
+    case 'auto':
+      show()
+      break
+
+    default:
+      document.addEventListener('keyup', showByKey)
+      break
   }
 
   this.destroy = () => {
     sceneElement.removeEventListener('click', show)
+    sceneElement.removeEventListener('click', hide)
     document.removeEventListener('keyup', showByKey)
   }
 }
